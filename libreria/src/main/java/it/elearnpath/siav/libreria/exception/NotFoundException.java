c6f677ead4a9865180ccb041fc684d10a9387fa4
andry1997 <Americanidiot.97>
2021-02-23 16:49:19 +0100

    aggiunta gestione delle eccezioni

---
+++ libreria/src/main/java/it/elearnpath/siav/libreria/exception/NotFoundException.java
@@ -0,0 +1,25 @@
+package it.elearnpath.siav.libreria.exception;
+
+public class NotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    private String messaggio = "Elemento Ricercato Non Trovato!";
+
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String Messaggio) {
+        super(Messaggio);
+        this.messaggio = Messaggio;
+    }
+
+    public String getMessaggio() {
+        return messaggio;
+    }
+
+    public void setMessaggio(String messaggio) {
+        this.messaggio = messaggio;
+    }
+
+}
