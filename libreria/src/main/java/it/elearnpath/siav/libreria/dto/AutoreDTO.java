114b9ae0d53ed6460a2bc9e7388d4f61c27dfdbe
ebolart <fantabosco993@gmail.com>
2021-02-23 15:50:34 +0100

    aggiungte Entity mappate, DTO e controller di prova

---
+++ libreria/src/main/java/it/elearnpath/siav/libreria/dto/AutoreDTO.java
@@ -0,0 +1,22 @@
+package it.elearnpath.siav.libreria.dto;
+
+import lombok.Data;
+
+@Data
+public class AutoreDTO {
+
+    private Integer idAutore;
+
+    private String nome;
+
+    private String cognome;
+
+    private String biografia;
+
+    private String dataNascita;
+
+    private String dataMorte;
+
+    private String nazionalita;
+
+}
