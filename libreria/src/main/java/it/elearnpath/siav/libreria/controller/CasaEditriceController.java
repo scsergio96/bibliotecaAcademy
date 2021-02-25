0dd6f92fb2656ffac528304e217d63fc74910fb5
Sergio Ciringione <scsergio96@gmail.com>
2021-02-25 09:35:22 +0100

    add modelmapper dependency

--- libreria/src/main/java/it/elearnpath/siav/libreria/controller/CasaEditriceController.java
+++ libreria/src/main/java/it/elearnpath/siav/libreria/controller/CasaEditriceController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponses;
 import it.elearnpath.siav.libreria.dto.CasaEditriceDTO;
