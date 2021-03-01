7aafc53b2d4815b35c53b3d99822fc8a0bb7ced2
andry1997 <Americanidiot.97>
2021-03-01 11:02:59 +0100

    refactoring

--- registry/src/main/java/it/elearnpath/siav/registry/controller/LoanController.java
+++ registry/src/main/java/it/elearnpath/siav/registry/controller/LoanController.java
@@ -3,14 +3,11 @@ package it.elearnpath.siav.registry.controller;
 import it.elearnpath.siav.registry.dto.AuthorDTO;
 import it.elearnpath.siav.registry.dto.LoanDTO;
 import it.elearnpath.siav.registry.service.LoanService;
-import org.springframework.http.HttpHeaders;
+import it.elearnpath.siav.registry.service.LoanServiceImpl;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- registry/src/main/java/it/elearnpath/siav/registry/service/LoanService.java
+++ registry/src/main/java/it/elearnpath/siav/registry/service/LoanService.java
@@ -1,43 +1,11 @@
 package it.elearnpath.siav.registry.service;
 
-import it.elearnpath.siav.registry.dto.LoanDTO;
-import it.elearnpath.siav.registry.entity.Loan;
-import it.elearnpath.siav.registry.repository.LoanRepository;
-import org.springframework.data.domain.Example;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Service
-public class LoanService {
+import it.elearnpath.siav.registry.dto.LoanDTO;
 
-    private final LoanRepository loanRepository;
+public interface LoanService {
 
-    public LoanService(LoanRepository loanRepository) {
-        this.loanRepository = loanRepository;
-    }
-
-    public List<LoanDTO> searchByReaderIdBookId(Integer readerId, Integer bookId) {
-        Loan loanExample = new Loan();
-        loanExample.setIdReader(readerId);
-        loanExample.setIdBook(bookId);
-
-        List<Loan> loans = loanRepository.findAll(Example.of(loanExample));
-        List<LoanDTO> loanDTOs = new ArrayList<>();
-
-        loanDTOs = loans.stream()
-            .map(loan -> {
-                LoanDTO loanDTO = new LoanDTO();
-                loanDTO.setId(loan.getId());
-                loanDTO.setIdReader(loan.getIdReader());
-                loanDTO.setIdBook(loan.getIdBook());
-                loanDTO.setStart(loan.getStart());
-                loanDTO.setEnd(loan.getEnd());
-                return loanDTO;
-            }).collect(Collectors.toList());
-
-        return loanDTOs;
-    }
+    List<LoanDTO> searchByReaderIdBookId(Integer readerId, Integer bookId);
+    
 }
